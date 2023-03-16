af967780189fe722235c06ec1984deb65e5add4a
Yueh-Hua Tu <yuehhua.tu@ailabs.tw>
2023-03-16 13:52:01 +0800

    add unnormalized (auto-)covariance

--- federatedprs/__init__.py
+++ federatedprs/__init__.py
@@ -1,5 +1,4 @@
 from .stats import (
     unnorm_autocovariance, unnorm_covariance,
-    vmap_unnorm_autocovariance, vmap_unnorm_covariance,
 )
 from .regression import FedLinearRegression
--- federatedprs/regression.py
+++ federatedprs/regression.py
@@ -1,6 +1,5 @@
 import numpy as np
-from jax import jit, vmap
-from jax import numpy as jnp
+from .stats import gen_mvdot
 
 class FedLinearRegression:
     
@@ -8,8 +7,5 @@ class FedLinearRegression:
         self.__beta = beta
     
     def predict(self, X: np.ndarray[(1, 1), np.floating]):
-        return jnp.dot(X, self.__beta)
-    
-    @jit
-    def vmap_predict(self, X: np.ndarray[(1, 1), np.floating]):
-        return vmap(self.predict)(X)
+        f = gen_mvdot(self.__beta)
+        return f(X)
