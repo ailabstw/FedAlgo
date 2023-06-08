038a17cb0a9b257aa68cf5094b99f01b24c36d6e
Yueh-Hua Tu <yuehhua.tu@ailabs.tw>
2023-06-08 14:55:19 +0800

    relax python version to 3.7 and extend unit test to latest

--- gwasprs/stats.py
+++ gwasprs/stats.py
@@ -3,11 +3,11 @@ from jax import jit, vmap
 from jax import numpy as jnp
 
 @jit
-def mvdot(X: np.ndarray[(1, 1), np.floating], y: np.ndarray[(1,), np.floating]) -> np.ndarray[(1,), np.floating]:
+def mvdot(X: 'np.ndarray[(1, 1), np.floating]', y: 'np.ndarray[(1,), np.floating]') -> 'np.ndarray[(1,), np.floating]':
     return vmap(jnp.vdot, (0, None), 0)(X, y)
 
 @jit
-def matmul(X: np.ndarray[(1, 1), np.floating], Y: np.ndarray[(1, 1), np.floating]) -> np.ndarray[(1, 1), np.floating]:
+def matmul(X: 'np.ndarray[(1, 1), np.floating]', Y: 'np.ndarray[(1, 1), np.floating]') -> 'np.ndarray[(1, 1), np.floating]':
     return vmap(mvdot, (None, 1), 1)(X, Y)
 
 def gen_mvdot(y: np.ndarray):
@@ -17,8 +17,8 @@ def gen_mvdot(y: np.ndarray):
     
     return _mvdot
 
-def unnorm_autocovariance(X: np.ndarray[(1, 1), np.floating]) -> np.ndarray[(1, 1), np.floating]:
+def unnorm_autocovariance(X: 'np.ndarray[(1, 1), np.floating]') -> 'np.ndarray[(1, 1), np.floating]':
     return matmul(X.T, X)
 
-def unnorm_covariance(X: np.ndarray[(1, 1), np.floating], y: np.ndarray[(1,), np.floating]):
+def unnorm_covariance(X: 'np.ndarray[(1, 1), np.floating]', y: 'np.ndarray[(1,), np.floating]'):
     return mvdot(X.T, y)
