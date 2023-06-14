68164a03fc25c4823800265d4e231b4dc38ed592
Yueh-Hua Tu <yuehhua.tu@ailabs.tw>
2023-06-14 15:34:57 +0800

    fix

--- gwasprs/mask.py
+++ gwasprs/mask.py
@@ -1,7 +1,7 @@
 import numpy as np
 from jax import numpy as jnp
 
-def get_mask(X: np.ndarray[(1, 1), np.floating]):
+def get_mask(X: 'np.ndarray[(1, 1), np.floating]'):
     return jnp.expand_dims(jnp.sum(jnp.isnan(X), axis = 1) == 0, -1)
 
 def nonnan_count(x):
