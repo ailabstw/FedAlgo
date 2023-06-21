83b988ab251112378e259a39c991796a92f0196a
Yueh-Hua Tu <yuehhua.tu@ailabs.tw>
2023-06-21 21:28:46 +0800

    add batched unnorm_autocovariance and unnorm_covariance

--- gwasprs/stats.py
+++ gwasprs/stats.py
@@ -7,13 +7,13 @@ def unnorm_autocovariance(X: 'np.ndarray[(1, 1), np.floating]') -> 'np.ndarray[(
     return linalg.mmdot(X, X)
 
 def unnorm_covariance(X: 'np.ndarray[(1, 1), np.floating]', y: 'np.ndarray[(1,), np.floating]'):
-    return linalg.mvmul(X.T, y)
+    return linalg.mvdot(X, y)
 
 def batched_unnorm_autocovariance(X: 'np.ndarray[(1, 1, 1), np.floating]') -> 'np.ndarray[(1, 1, 1), np.floating]':
     return linalg.batched_mmdot(X, X)
 
 def batched_unnorm_covariance(X: 'np.ndarray[(1, 1, 1), np.floating]', y: 'np.ndarray[(1, 1), np.floating]'):
-    return linalg.batched_mvmul(X.T, y)
+    return linalg.batched_mvdot(X, y)
 
 def t_dist_pvalue(t_stat, df):
     return 2 * (1 - stats.t.cdf(np.abs(t_stat), df))
