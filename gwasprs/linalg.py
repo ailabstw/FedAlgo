576f491874cb10b3b5cc4127bde60f6ddcd88dd8
Yueh-Hua Tu <yuehhua.tu@ailabs.tw>
2023-06-21 21:00:35 +0800

    add docstring

--- gwasprs/linalg.py
+++ gwasprs/linalg.py
@@ -4,14 +4,47 @@ from jax import numpy as jnp
 
 @jit
 def mvdot(X: 'np.ndarray[(1, 1), np.floating]', y: 'np.ndarray[(1,), np.floating]') -> 'np.ndarray[(1,), np.floating]':
+    """Matrix-vector multiplication
+    
+    Perform X * y.
+
+    Args:
+        X (np.ndarray[(1, 1), np.floating]): Matrix.
+        y (np.ndarray[(1,), np.floating]): Vector.
+
+    Returns:
+        np.ndarray[(1,), np.floating]: Vector.
+    """
     return vmap(jnp.vdot, (0, None), 0)(X, y)
 
 @jit
 def mmdot(X: 'np.ndarray[(1, 1), np.floating]', Y: 'np.ndarray[(1, 1), np.floating]') -> 'np.ndarray[(1, 1), np.floating]':
+    """Matrix-matrix dot product
+    
+    Perform X.T * Y.
+
+    Args:
+        X (np.ndarray[(1, 1), np.floating]): Matrix.
+        Y (np.ndarray[(1, 1), np.floating]): Matrix.
+
+    Returns:
+        np.ndarray[(1, 1), np.floating]: Matrix.
+    """
     return vmap(mvdot, (None, 1), 1)(X.T, Y)
 
 @jit
 def matmul(X: 'np.ndarray[(1, 1), np.floating]', Y: 'np.ndarray[(1, 1), np.floating]') -> 'np.ndarray[(1, 1), np.floating]':
+    """Matrix multiplication
+    
+    Perform X * Y.
+
+    Args:
+        X (np.ndarray[(1, 1), np.floating]): Matrix.
+        Y (np.ndarray[(1, 1), np.floating]): Matrix.
+
+    Returns:
+        np.ndarray[(1, 1), np.floating]: Matrix.
+    """
     return vmap(mvdot, (None, 1), 1)(X, Y)
 
 def gen_mvdot(y: np.ndarray):
@@ -23,12 +56,45 @@ def gen_mvdot(y: np.ndarray):
 
 @jit
 def batched_mvdot(X: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """Batched matrix-vector multiplication
+    
+    Perform X * y with batch on their last dimension.
+
+    Args:
+        X (np.ndarray[(1, 1, 1), np.floating]): Batched matrix.
+        y (np.ndarray[(1, 1), np.floating]): Batched vector.
+
+    Returns:
+        np.ndarray[(1, 1), np.floating]: Batched vector.
+    """
     return vmap(mvdot, (2, 1), 1)(X, y)
 
 @jit
 def batched_mmdot(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+    """Batched matrix-matrix dot product
+    
+    Perform X.T * Y with batch on their last dimension.
+
+    Args:
+        X (np.ndarray[(1, 1, 1), np.floating]): Batched matrix.
+        Y (np.ndarray[(1, 1, 1), np.floating]): Batched matrix.
+
+    Returns:
+        np.ndarray[(1, 1, 1), np.floating]: Batched matrix.
+    """
     return vmap(mmdot, 2, 2)(X, Y)
 
 @jit
 def batched_matmul(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+    """Batched matrix multiplication
+    
+    Perform X * Y with batch on their last dimension.
+
+    Args:
+        X (np.ndarray[(1, 1, 1), np.floating]): Batched matrix.
+        Y (np.ndarray[(1, 1, 1), np.floating]): Batched matrix.
+
+    Returns:
+        np.ndarray[(1, 1, 1), np.floating]: Batched matrix.
+    """
     return vmap(matmul, 2, 2)(X, Y)
