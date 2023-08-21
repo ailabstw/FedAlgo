1ae99a7c4e87b97f6846ae47fff60333413b4c76
Yueh-Hua Tu <yuehhua.tu@ailabs.tw>
2023-08-21 10:06:47 +0800

    draft for block diag

--- gwasprs/stats.py
+++ gwasprs/stats.py
@@ -77,6 +77,12 @@ def batched_unnorm_autocovariance(X: 'np.ndarray[(1, 1, 1), np.floating]', accel
 def batched_unnorm_covariance(X: 'np.ndarray[(1, 1, 1), np.floating]', y: 'np.ndarray[(1, 1), np.floating]'):
     return linalg.batched_mvdot(X, y)
 
+def blocked_unnorm_autocovariance(X: 'np.ndarray[(1, 1), np.floating]') -> 'np.ndarray[(1, 1), np.floating]':
+    return X.T @ X
+
+def blocked_unnorm_covariance(X: 'np.ndarray[(1, 1), np.floating]', y: 'np.ndarray[(1,), np.floating]'):
+    return X.T @ y
+
 def t_dist_pvalue(t_stat, df):
     return 2 * (1 - stats.t.cdf(np.abs(t_stat), df))
 
